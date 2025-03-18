0ec5894c65121e51669747f8c2d7a49b06148a18
narendarpaindla <narendarreddypaindla@gmail.com>
2025-03-18 04:01:38 +0000

    commit

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -10,7 +10,7 @@ import com.example.repository.UserRepository;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService { 
 
      @Autowired
      private UserRepository userRepository;
